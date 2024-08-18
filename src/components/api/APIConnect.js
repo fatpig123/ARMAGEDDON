65e45c262ee822ca80d46b09c4bd00f61a471e08
SY-GG <sy.park.HUB@gmail.com>
2024-08-18 14:31:03 +0900

    파일 이미지 base64 변경 후 openai 호출

--- src/components/api/APIConnect.js
+++ src/components/api/APIConnect.js
@@ -1,7 +1,53 @@
-export const CallGPT = async () => {
+export const CallGPT = async (imgUrl) => {
   // test 세팅
-  const org_file_name = "스타벅스 메인 수정본";
   const file_date = new Date().toLocaleString();
+
+  const message_list= [
+    {
+      role: "system",
+      content:
+        "너는 파일명을 만들어주는 기계야. 컴퓨터 지식이 전혀 없는 사람들 대신 컴퓨터 문법을 가진 파일명을 만들어줘.",
+    },
+    {
+      role: "assistant",
+      content: "파일명은 날짜, 파일명(프로젝트), 상세정보 순서로 기입합니다. 각 요소를 섞거나 쪼개쓰지 않습니다.",
+    },
+    {
+      role: "assistant",
+      content: "파일명은 무조건 영어로 지어야합니다.",
+    },
+    { role: "user", content: "파일명에 들어갈 날짜야:" + file_date },
+    {
+      role: "user",
+      content: "파일명(프로젝트)은 실제 존재하는 고유명사인지 한번 확인해. 확인이 안 된다면 로마자 변환을 해서 써.",
+    },
+    {
+      role: "user",
+      content:
+        "상세정보(키워드)에 대한 유의사항이 있어. 1)모든 상세정보는 숫자나 산술식이 아니라 그저 문자열이야. 2)각 키워드는 띄어쓰기를 하지말고 한 단어로 써줘. 3)각 키워드는 10바이트 이하로 줄여줘. 4)날짜 뒤에 구분자 혹은 특수문자가 있다면 띄어쓰기를 하지 않아도 돼.",
+    },
+    {
+      role: "user",
+      content:
+        "모든 단어가 고유명사인지 한번 확인해. 검색해보고 존재하는 회사명, 프로젝트 등 고유명사라면 검색된 단어를 써.",
+    },
+    {
+      role: "user",
+      content: "복사하기 쉽게 파일명만 답해줘. 예=> [2024.05.23]hello_world_computer_2+1sale",
+    },
+  ];
+
+  message_list.push({
+    role: "user",
+    content: [
+    {
+    "type": "text",
+            "text" : "이 그림을 보고 파일명을 지어줘."   
+    },    
+    {
+    "type": "image_url",
+            "image_url" : { "url": `${imgUrl}`}   
+    }]});
 
   const response = await fetch("https://api.openai.com/v1/chat/completions", {
     method: "POST",
@@ -11,50 +57,14 @@ export const CallGPT = async () => {
     },
     body: JSON.stringify({
       model: "gpt-4o-mini",
-      messages: [
-        {
-          role: "system",
-          content:
-            "너는 파일명을 만들어주는 기계야. 컴퓨터 지식이 전혀 없는 사람들 대신 컴퓨터 문법을 가진 파일명을 만들어줘.",
-        },
-        {
-          role: "assistant",
-          content: "파일명은 날짜, 파일명(프로젝트), 상세정보 순서로 기입합니다. 각 요소를 섞거나 쪼개쓰지 않습니다.",
-        },
-        {
-          role: "assistant",
-          content: "파일명은 무조건 영어로 지어야합니다.",
-        },
-        { role: "user", content: "파일명에 들어갈 날짜야:" + file_date },
-        {
-          role: "user",
-          content: "파일명(프로젝트)은" + org_file_name + "이야. 이것의 파일명을 만들어줘.",
-        },
-        {
-          role: "user",
-          content: "파일명(프로젝트)은 실제 존재하는 고유명사인지 한번 확인해. 확인이 안 된다면 로마자 변환을 해서 써.",
-        },
-        {
-          role: "user",
-          content:
-            "상세정보(키워드)에 대한 유의사항이 있어. 1)모든 상세정보는 숫자나 산술식이 아니라 그저 문자열이야. 2)각 키워드는 띄어쓰기를 하지말고 한 단어로 써줘. 3)각 키워드는 10바이트 이하로 줄여줘. 4)날짜 뒤에 구분자 혹은 특수문자가 있다면 띄어쓰기를 하지 않아도 돼.",
-        },
-        {
-          role: "user",
-          content:
-            "모든 단어가 고유명사인지 한번 확인해. 검색해보고 존재하는 회사명, 프로젝트 등 고유명사라면 검색된 단어를 써.",
-        },
-        {
-          role: "user",
-          content: "복사하기 쉽게 파일명만 답해줘. 예=> [2024.05.23]hello_world_computer_2+1sale",
-        },
-      ],
+      messages: message_list,
       temperature: 0,
       max_tokens: 30,
     }),
   });
 
   const responseData = await response.json();
+  console.log("imgUrl", imgUrl);
   console.log("responseData : ", responseData);
   console.log("responseDataContent : \n", responseData.choices[0].message.content);
 
--- src/transform.js
+++ src/transform.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState, useEffect }from "react";
 import { CallGPT } from "./components/api/APIConnect";
+import FileBase64 from 'react-file-base64';
 
 function Transform(props) {
+  const [imgList, setImgList] = useState([]);
+  
     const transform = async () =>{
       let files = props.files;
       let items = props.items;
       let lang = props.lang;
       let formData = props.formData;
 
+      const nowImageURLList = [];
       alert(JSON.stringify(items));
       alert(lang);
       alert(JSON.stringify(formData));
@@ -16,26 +20,41 @@ function Transform(props) {
         const filetype = index !== -1 ? files[i].type.substring(0, index) : files[i].type;
         if(filetype == 'image'){
           try {
-            const formData = new FormData();
-            formData.append('file', files[i]);
-            console.log(files[i]);
-            const response = await fetch("http://localhost:5000/ask", {
-              method: "POST",
-              body: formData,
-            });
-
-            const data = await response.json(); // JSON 응답을 파싱
-            alert(data.answer)
-
-          } catch (error) { 
+            // const nowImageUrl = URL.createObjectURL(nowSelectImageList[i]);
+            const convertedFile = await convertToBase64(files[i]);
+            nowImageURLList.push(convertedFile);
+          } catch (error) {
             console.error("서버로 파일 전송 중 오류가 발생했습니다:", error);
           }
         }
         else if(files[i].type == 'application/pdf'){
           
         }
-      } 
+      }
+      setImgList([...nowImageURLList]);
     }
+  // OpenAI API호출
+  useEffect(() => {
+    for (let imgUrl of imgList){
+      handleClickAPICall(imgUrl);
+    }
+  },[imgList]);
+
+  //base64로 파일 변경
+  function convertToBase64(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+  
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = (error) => reject(error);
+  
+      reader.readAsDataURL(file);
+    });
+  }
+
+    const handleClickAPICall = async (imgUrl) => {
+      await CallGPT(imgUrl);
+    };
 
     return (
       <div className="header">
