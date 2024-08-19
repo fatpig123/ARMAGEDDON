ab47d2cac50dfeb0a432ab484699248897ad8b0a
fatpig123 <joilwoo123@gmail.com>
2024-08-19 09:51:01 +0900

    css 수정

---
+++ src/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+        }
+    });
+
+    win.loadURL('http://localhost:3000'); // React 개발 서버를 가리킵니다.
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+    win.loadFile(path.join(__dirname, 'build/index.html'));
+
+});
